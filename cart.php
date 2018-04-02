e4415744ad2d455b262a9f711bf5627efb0b2529
Kenny Nguyen <31287213+Kendngu@users.noreply.github.com>
2018-04-02 17:15:46 -0600

    views

---
+++ cart.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title> Shopping Cart </title>
+
+</head>
+<body>
+
+	<div class="container text-center">
+	  <h3> Shopping Cart</h3>
+		<p>
+			Items in cart:
+       </p>
+</div>
+</body>
+</html>
